2c2689e38d449f43de8832bee2d43f86061e47ba
Michael Yuan <michaelyuan1@yahoo.com>
2012-03-11 01:38:02 -0500

    done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,10 @@
 module ApplicationHelper
   def title
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "GroupText"
     if @title.nil?
       base_title
     else
       "#{base_title} | #{@title}"
     end
-  end
-  
-  def logo
-    image_tag("logo.png", :alt => "Sample App", :class => "round")
   end
 end
